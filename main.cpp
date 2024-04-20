cd201810c52f40e5f1ee9a52af2cd915c2f46814
Danial Al Ghazali Walangadi <danialagw@students.unnes.ac.id>
2024-04-20 18:43:35 +0700

    Nothing of note

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 void RREF(Eigen::MatrixXd& matrix) {
     int lead = 0;
-    int rowCount = matrix.rows();
-    int colCount = matrix.cols();
+    long long rowCount = matrix.rows();
+    long long colCount = matrix.cols();
 
     for(int r = 0; r < rowCount; ++r){
         if (colCount <= lead)
@@ -64,8 +64,8 @@ int main() {
 
     cols = 0;
     rows = 0;
-    for(std::string s : sectors){
-        for(std::string ss : sectors){
+    for(std::string& s : sectors){
+        for(std::string& ss : sectors){
             std::cout<<"What is the output from "<<s<<" to "<<ss<<" ? (in %)\n";
             std::cin>>tempOutput;
             initialMatrix(rows,cols) = tempOutput;
@@ -93,7 +93,7 @@ int main() {
     for(int i = 0 ; i < sectorPopulation-1 ; i++) {
         for(int ii = 0 ; ii < sectorPopulation ; ii++) {
             if(resultMatrix(i, ii) == 1){
-                std::cout<<"i = "<<i<<" , ii = "<<ii<<std::endl;
+                //std::cout<<"i = "<<i<<" , ii = "<<ii<<std::endl;
                 std::cout<<resultMatrix(i,ii)<<" product from sector "<<sectors[ii]<<" is equal to "<<
                          scalar*resultMatrix(i, sectorPopulation-1)<<" product from sector "<<sectors[sectorPopulation-1]
                          <<std::endl;
